0409cb9a9a234a56b1774a78cb6188c7e176af1b
Diana Sosnovskaya <progdidi@gmail.com>
2024-01-20 18:33:00 +0100

    refactor: add revieSection component

---
+++ src/components/reviewSection/ReviewSection.js
@@ -0,0 +1,35 @@
+import './reviewSection.scss';
+
+import ReviewsItem from '../reviewsItem/ReviewsItem';
+import { reviews } from '../../helpers/reviews/reviews';
+
+const ReviewSection = () => {
+    return ( 
+        <section className="reviews">
+            <div className="container">
+                <div className="reviews__inner">
+                    <div className="reviews__info">
+                        <h3 className="section__title reviews__title">Отзывы</h3>
+                        <p className="reviews__descr">Lorem Ipsum is simply dummy text of the printing and typesetting industry</p>
+                        <button className="reviews__btn main-btn">Оставить отзыв</button>
+                    </div>
+                    {reviews.map((review) => {
+                        return (
+                            <ReviewsItem
+                                name={review.name}
+                                tour={review.tour}
+                                text={review.text}
+                                rate={review.rate}
+                                key={review.id}
+                                img={review.img}
+                            />
+                        )
+                    })}
+                </div>
+                
+            </div>
+        </section>
+     );
+}
+ 
+export default ReviewSection;
--- src/pages/mainPage/MainPage.js
+++ src/pages/mainPage/MainPage.js
@@ -10,10 +10,9 @@ import {directions} from "../../helpers/directions/directions";
 import ExcursionItem from "../../components/excursionItem/ExcursionItem";
 import {excursions} from "../../helpers/excursions/excursions";
 
-import ReviewsItem from "../../components/reviewsItem/ReviewsItem";
-import { reviews } from '../../helpers/reviews/reviews';
-
 import BlogSection from '../../components/blogSection/BlogSection';
+
+import ReviewSection from '../../components/reviewSection/ReviewSection';
 
 const MainPage = () => {
     return ( 
@@ -63,32 +62,9 @@ const MainPage = () => {
                 </div>
             </section>
 
-            <section className="reviews">
-                <div className="container">
-                    <div className="reviews__inner">
-                        <div className="reviews__info">
-                            <h3 className="section__title reviews__title">Отзывы</h3>
-                            <p className="reviews__descr">Lorem Ipsum is simply dummy text of the printing and typesetting industry</p>
-                            <button className="reviews__btn main-btn">Оставить отзыв</button>
-                        </div>
-                        {reviews.map((review) => {
-                            return (
-                                <ReviewsItem
-                                    name={review.name}
-                                    tour={review.tour}
-                                    text={review.text}
-                                    rate={review.rate}
-                                    key={review.id}
-                                    img={review.img}
-                                />
-                            )
-                        })}
-                    </div>
-                    
-                </div>
-            </section>
+            <ReviewSection></ReviewSection>
 
-            <BlogSection/>
+            <BlogSection></BlogSection>
 
 
         </>
