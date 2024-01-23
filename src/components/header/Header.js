0d2f57f4c7efcd788dcf2d1927950afacbcf5731
Diana Sosnovskaya <progdidi@gmail.com>
2024-01-23 19:27:57 +0300

    feat: add header submenu

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,4 +1,5 @@
 import { NavLink } from 'react-router-dom';
+import { useState } from 'react';
 
 
 //images
@@ -9,16 +10,41 @@ import arr from './images/arr.svg';
 import './header.scss';
 
 const Header = () => {
+
+    const [isNavExpanded, setIsNavExpanded] = useState(false);
+
+
+
     return ( 
         <header className="header">
             <div className="container">
                 <div className="header__inner">
-                    <a href="" className="logo"><img src={logo} alt="" className="logo__img" /></a>
+                    <NavLink to="/" className="logo"><img src={logo} alt="" className="logo__img" /></NavLink>
 
                     <nav className="menu">
                         <ul className="menu__list">
-                            <li className="menu__list-item">
-                                <a href="" className="menu__list-link">Направления <img src={arr} alt="" className="arr_img" /></a>
+                            <li className="menu__list-item direction" onClick={() => {setIsNavExpanded(!isNavExpanded)}}>
+                                <button className="menu__list-link">Направления <img src={arr} alt="" className="arr_img" /></button>
+                                <ul className={isNavExpanded ? "submenu" : "submenu active"}>
+                                    <li className="submenu__item">
+                                        <NavLink to="/excursion" className="menu__list-link">Дубай</NavLink>
+                                    </li>
+                                    <li className="submenu__item">
+                                        <NavLink to="/excursion" className="menu__list-link">Стамбул</NavLink>
+                                    </li>
+                                    <li className="submenu__item">
+                                        <NavLink to="/excursion" className="menu__list-link">Санкт-Петербург</NavLink>
+                                    </li>
+                                    <li className="submenu__item">
+                                        <NavLink to="/excursion" className="menu__list-link">Карелия</NavLink>
+                                    </li>
+                                    <li className="submenu__item">
+                                        <NavLink to="/excursion" className="menu__list-link">Калининград</NavLink>
+                                    </li>
+                                    <li className="submenu__item">
+                                        <NavLink to="/excursion" className="menu__list-link">Мурманск</NavLink>
+                                    </li>
+                                </ul>
                             </li>
                             <li className="menu__list-item">
                                 <NavLink to="/about" className="menu__list-link">О сервисе</NavLink>
