ebb570db9732fe7d8981876433165dea387830ce
Diana Sosnovskaya <progdidi@gmail.com>
2024-01-24 16:59:39 +0300

    fix: fix blogpage styles

--- src/pages/blogPage/BlogPage.js
+++ src/pages/blogPage/BlogPage.js
@@ -13,8 +13,16 @@ const BlogPage = () => {
             <section className="blog-page">
                 <div className="container">
                     <h2 className="blog-page__title page__title">Блог</h2>
-                    <p className="blog-page__descr">платформа простого и надежного онлайн бронирования экскурсий на русском языке по всему миру. Был основан в 2012 году в Санкт-Петербурге.</p>
+                    <div className="blog-page__top">
+                        <p className="blog-page__descr">платформа простого и надежного онлайн бронирования экскурсий на русском языке по всему миру. Был основан в 2012 году в Санкт-Петербурге.</p>
 
+                        <div className="blog-page__sort">
+                            <p className="blog-page__sort-text">Сортировать</p>
+                            <button className="blog-page__sort-btn">По умолчанию</button>
+                        </div>
+                    </div>
+                    
+                    
                     <div className="blog-page__items">
                         {blogs.map((blog) => {
                             return (
