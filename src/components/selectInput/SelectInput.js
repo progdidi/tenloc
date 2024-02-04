e934ba45402ea73e86f59dcbf10ff56b64fb5adb
Diana Sosnovskaya <progdidi@gmail.com>
2024-02-04 16:18:21 +0300

    add gh-pages

--- src/components/selectInput/SelectInput.js
+++ src/components/selectInput/SelectInput.js
@@ -1,9 +1,8 @@
 import './selectInput.scss';
 
-const SelectInput = ({value, options}) => {
+const SelectInput = ({value, options, title}) => {
     return ( 
         <select value={value} name="" id="" className="select-input">
-
                 {options.map((option) => (
                     <option value={option.value} className="select-input__item">{option.label}</option>
                 ))}
